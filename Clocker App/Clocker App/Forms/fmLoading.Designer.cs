6dd970a5d69e8d00d2818d0b5904c3df52b1acec
James Cockfield <jamescockfield10@gmail.com>
2021-12-16 00:04:03 +0000

    Loading Form

---
+++ Clocker App/Clocker App/Forms/fmLoading.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Clocker_App
+{
+    partial class fmLoading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbLoading = new System.Windows.Forms.ProgressBar();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pbLoading
+            // 
+            this.pbLoading.Location = new System.Drawing.Point(121, 313);
+            this.pbLoading.Name = "pbLoading";
+            this.pbLoading.Size = new System.Drawing.Size(549, 46);
+            this.pbLoading.Step = 1;
+            this.pbLoading.TabIndex = 0;
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoading.Location = new System.Drawing.Point(284, 240);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(0, 33);
+            this.lblLoading.TabIndex = 1;
+            // 
+            // fmLoading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.pbLoading);
+            this.Name = "fmLoading";
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pbLoading;
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Clocker App/Clocker App/Forms/fmLoading.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace Clocker_App
+{
+    public partial class fmLoading : Form
+    {
+        public fmLoading()
+        {
+            InitializeComponent();
+            InitializeMyTimer();
+        }
+
+
+        private void InitializeMyTimer()
+        {
+
+            timer1.Interval = 250;
+
+            timer1.Tick += new EventHandler(IncreaseProgressBar);
+
+            timer1.Start();
+        }
+
+        private void IncreaseProgressBar(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            pbLoading.Increment(r.Next(0,10));
+
+            lblLoading.Text = pbLoading.Value.ToString() + "% Completed";
+
+            if (pbLoading.Value == pbLoading.Maximum)
+            {
+                // Stop the timer.
+                timer1.Stop();
+                Thread.Sleep(1000);
+                this.Hide();
+                Thread.Sleep(1000);
+                fmHome h = new fmHome();
+                h.Show();
+            }
+        }
+    }
+}
